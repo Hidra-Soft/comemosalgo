59427ad950c9f5e53496207c3524900c3eff2857
flaviopifiator <flaviomariano1993@hotmail.com>
2016-10-08 21:45:19 -0300

    base de datos

--- comemosalgo/settings.py
+++ comemosalgo/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'comemosalgo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql_cymysql',
+        'NAME': 'comemosalgo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
