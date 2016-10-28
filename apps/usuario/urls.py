c67fc0547e2be708d4b616b2f2ec40da65dd9a30
educlerici <educlerici@gmail.com>
2016-10-28 19:57:04 -0300

    creacion template login, modificacion iniciar sesion y registrarse en template base

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from apps.usuario.views import RegistroUsuario
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^registrar/$', RegistroUsuario.as_view(), name='registrar')
