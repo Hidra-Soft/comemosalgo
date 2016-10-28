24a21f224798e42cf6481e07b31f4d21e8c6d675
educlerici <educlerici@gmail.com>
2016-10-28 17:47:21 -0300

    Se realizo el formulario de registro de usuario, ademas se creo el forms.py, y se modifico la view basada en clases

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
-
+from apps.usuario.forms import RegistroForm
 
 class RegistroUsuario(CreateView):
     model = User
     template_name = "registrar.html"
-    form_class = UserCreationForm
+    form_class = RegistroForm
     success_url = reverse_lazy('index:index')
 
 
