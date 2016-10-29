59832621b975905bec3f422a1666dbae1c19dbab
educlerici <educlerici@gmail.com>
2016-10-29 18:57:24 -0300

    se agrego template lista comercios y detalle de comercio

--- apps/comercio/urls.py
+++ apps/comercio/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from apps.comercio.views import index
+from apps.comercio.views import ListaComercio, DetalleComercio
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'^lista$', ListaComercio.as_view(), name='lista'),
+    url(r'^detalle/(?P<pk>[0-9]+)/$', DetalleComercio.as_view(), name='detalle')
 ]
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
